a5de53dbdf579ba642b4b07e433d2da54519df34
gaspno <zatchgabriel@gmail.com>
2022-06-26 19:36:41 -0300

    Password Generation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  length=0
   title = 'PasswordGeneration';
+  password=''
+  value=0
+  letterChecked=false
+  numberChecked=false
+  symbolChecked=false
+
+    onGenerateButtonClick(){
+      const letters="abcdefghijklmnopqrstuvwyz"
+      const numbers="0123456789"
+      const symbols="!#@%¨&*()_-+=-*/,>:;?|'\""
+
+      let validChars=''
+      if(this.letterChecked){
+        validChars+=letters
+      }
+      if(this.numberChecked){
+        validChars+=numbers
+      }
+      if(this.symbolChecked){
+        validChars+=symbols
+      }
+      if(validChars!==''){
+      this.generatePassword(validChars)
+      }
+      else{
+        alert('marque uma opção')
+      }
+    
+    }
+    onChangeLetters(){
+      this.letterChecked=!this.letterChecked
+    }
+    onChangeSymbols(){
+      this.symbolChecked=!this.symbolChecked
+    }
+    onChangeNumbers(){
+      this.numberChecked=!this.numberChecked
+    }
+    onChangeLength(event:Event){
+
+      const value = (event.target as HTMLInputElement).value
+      this.length=parseInt(value)
+            
+    }
+    generatePassword(validChars:string){
+
+      let password=''
+      for(let i=0;i<this.length;i++){
+        password+=validChars[Math.floor(Math.random() *validChars.length)]
+      }
+      this.password=password
+      
+    }
 }
